49991122a9830ac9c3b6fb756e2db934265ab974
liza0316 <zinkina03@gmail.com>
2025-02-18 20:54:06 +0200

    add all

---
+++ 1.js
@@ -0,0 +1,9 @@
+let userPassword = Number(prompt("please enter your password", "111"));
+
+if (userPassword === 111) {
+    alert("access level A");
+} else if (userPassword === 222) {
+    alert("access level B");
+} else {
+    alert("Access is prohibited!!!");
+}
